6d81707745724e59b3161a30d7145e79424475d8
aryansonid <aryan.s400029@gmail.com>
2021-11-14 03:43:54 +0530

    refactor: extract logging middleware to middleware package

---
+++ middleware/logging.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+// Logging logs method, path, status, and duration for each request.
+func Logging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wrapped := &statusWriter{ResponseWriter: w, status: http.StatusOK}
+		next.ServeHTTP(wrapped, r)
+		log.Printf("%s %s %d %s", r.Method, r.URL.Path, wrapped.status, time.Since(start))
+	})
+}
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (sw *statusWriter) WriteHeader(code int) {
+	sw.status = code
+	sw.ResponseWriter.WriteHeader(code)
+}
