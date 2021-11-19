1818eaac7b8ecc3c1e7470e829846f591e40df82
aryansonid <aryan.s400029@gmail.com>
2021-11-19 18:25:21 +0530

    feat: add recovery middleware for panic handling

---
+++ middleware/recovery.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"runtime/debug"
+)
+
+// Recovery catches panics and returns 500 instead of crashing.
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic recovered: %v\n%s", err, debug.Stack())
+				http.Error(w, "internal server error", http.StatusInternalServerError)
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
