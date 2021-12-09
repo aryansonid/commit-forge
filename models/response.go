815dc9451d05323a1b9e07c5ac48547f104c78f7
aryansonid <aryan.s400029@gmail.com>
2021-12-10 04:07:25 +0530

    feat: add response models for API endpoints

---
+++ models/response.go
@@ -0,0 +1,26 @@
+package models
+
+// APIError represents a JSON error response.
+type APIError struct {
+	Error string `json:"error"`
+}
+
+// APISuccess represents a JSON success response.
+type APISuccess struct {
+	Message string `json:"message"`
+}
+
+// VersionInfo represents the version endpoint response.
+type VersionInfo struct {
+	Version string `json:"version"`
+	Time    string `json:"time"`
+}
+
+// RouteInfo represents the root endpoint response.
+type RouteInfo struct {
+	Message       string `json:"message"`
+	HealthRoute   string `json:"health_route"`
+	VersionRoute  string `json:"version_route"`
+	RewriteRoute  string `json:"rewrite_route"`
+	ExampleMethod string `json:"example_method"`
+}
