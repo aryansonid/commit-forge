a216ccfce823a3fb5371dd2ed0c24ad8badfd4ab
aryansonid <aryan.s400029@gmail.com>
2021-12-24 09:13:25 +0530

    feat: register /status route

--- routes/routes.go
+++ routes/routes.go
@@ -12,5 +12,6 @@ func Register(mux *http.ServeMux) {
 	mux.HandleFunc("/healthz", handlers.Health)
 	mux.HandleFunc("/readyz", handlers.Ready)
 	mux.HandleFunc("/version", handlers.Version)
+	mux.HandleFunc("/status", handlers.Status)
 	mux.HandleFunc("/rewrite-commits", handlers.Rewrite)
 }
