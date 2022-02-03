419166e2ad095d243a276f063e70a1d737c9de9f
aryansonid <aryan.s400029@gmail.com>
2022-02-03 09:58:21 +0530

    test: add CORS middleware unit tests

---
+++ middleware/cors_test.go
@@ -0,0 +1,40 @@
+package middleware
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCORSMiddleware(t *testing.T) {
+	handler := CORS("*")(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Header().Get("Access-Control-Allow-Origin") != "*" {
+		t.Error("CORS origin header not set")
+	}
+}
+
+func TestCORSPreflight(t *testing.T) {
+	handler := CORS("https://example.com")(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	req := httptest.NewRequest(http.MethodOptions, "/", nil)
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusNoContent {
+		t.Errorf("expected status 204 for preflight, got %d", rr.Code)
+	}
+	if rr.Header().Get("Access-Control-Allow-Origin") != "https://example.com" {
+		t.Error("CORS origin not set correctly for preflight")
+	}
+}
