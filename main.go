f1618dc86b701df4cfd60b60e17cc24d0989f304
aryansonid <aryan.s400029@gmail.com>
2022-02-23 11:24:38 +0530

    refactor: update main.go to load and validate config

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"log"
 
+	"commit-forge/config"
 	"commit-forge/server"
 )
 
 func main() {
-	srv := server.New()
+	cfg := config.Load()
+	if err := cfg.Validate(); err != nil {
+		log.Fatalf("invalid configuration: %v", err)
+	}
+
+	srv := server.New(cfg)
 	if err := srv.Start(); err != nil {
 		log.Fatalf("server failed: %v", err)
 	}
