46d6c823c298eac8771fc4e6fbe29a46971ef916
aryansonid <aryan.s400029@gmail.com>
2022-02-28 20:24:54 +0530

    feat: add config.Load() with environment variable parsing

--- config/config.go
+++ config/config.go
@@ -14,3 +14,45 @@ type Config struct {
 	MaxRetryAttempts  int
 	CORSAllowOrigins string
 }
+
+// Load reads configuration from environment variables with defaults.
+func Load() *Config {
+	return &Config{
+		Port:              getEnv("PORT", "8080"),
+		LogLevel:          getEnv("LOG_LEVEL", "info"),
+		RequestTimeout:    getDurationEnv("REQUEST_TIMEOUT", 60*time.Minute),
+		MaxRetryAttempts:  getIntEnv("MAX_RETRY_ATTEMPTS", 5),
+		CORSAllowOrigins: getEnv("CORS_ALLOWED_ORIGINS", "*"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return fallback
+}
+
+func getIntEnv(key string, fallback int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return fallback
+	}
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		return fallback
+	}
+	return i
+}
+
+func getDurationEnv(key string, fallback time.Duration) time.Duration {
+	v := os.Getenv(key)
+	if v == "" {
+		return fallback
+	}
+	d, err := time.ParseDuration(v)
+	if err != nil {
+		return fallback
+	}
+	return d
+}
